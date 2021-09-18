5b350142b101a65e884a1676224493fb6ea4ec04
cxnzensei <nishitpatil3231@gmail.com>
2021-09-18 12:12:27 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-let api_key = '4ba0d7eb9cf6e4d9ffdd61dfab171992';
-let searchInput = document.querySelector('#searchInput');
-let searchBtn = document.querySelector('#searchBtn');
-let celsius = document.querySelector('#celsius');
-let farenheit = document.querySelector('#farenheit');
+let api_key = "4ba0d7eb9cf6e4d9ffdd61dfab171992";
+let searchInput = document.querySelector("#searchInput");
+let searchBtn = document.querySelector("#searchBtn");
+let celsius = document.querySelector("#celsius");
+let farenheit = document.querySelector("#farenheit");
 
-searchBtn.addEventListener('click', getData);
-searchBtn.addEventListener('click', () => {
-    searchInput.value = ``;
-    celsius.classList.remove('chosen');
-    farenheit.classList.remove('chosen');
+searchBtn.addEventListener("click", getData);
+searchBtn.addEventListener("click", () => {
+    searchInput.value = "";
+    celsius.classList.remove("chosen");
+    farenheit.classList.remove("chosen");
 });
 
 async function getData() {
@@ -19,8 +19,7 @@ async function getData() {
         showData(weatherData);
     }
     catch {
-        console.log("Enter something")
-        alert("Type something that would make sense or check for spell errors!")
+        alert("Type something that would make sense or check for spell errors!");
     }
 }
 
@@ -33,52 +32,52 @@ function showData(obj) {
     let lat = obj.coord.lat;
 
     let tempF = obj.main.temp;
-    let feels_likeF = obj.main['feels_like'];
-    let min_tempF = obj.main['temp_min'];
-    let max_tempF = obj.main['temp_max'];
-    let api_pressure = obj.main['pressure'];
-    let api_humidity = obj.main['humidity'];
+    let feels_likeF = obj.main["feels_like"];
+    let min_tempF = obj.main["temp_min"];
+    let max_tempF = obj.main["temp_max"];
+    let api_pressure = obj.main["pressure"];
+    let api_humidity = obj.main["humidity"];
 
     let windSpeedApi = obj.wind.speed;  
     let windDegApi = obj.wind.deg;
 
     let date = new Date();
 
-    document.getElementById('time').textContent = date;
-    document.getElementById('h2').textContent = description; 
-    document.getElementById('place').textContent = `${locat}, ${country}`;
-    document.getElementById('coordinates').textContent = `${lat}°, ${lon}°`;
+    document.getElementById("time").textContent = date;
+    document.getElementById("h2").textContent = description; 
+    document.getElementById("place").textContent = `${locat}, ${country}`;
+    document.getElementById("coordinates").textContent = `${lat}°, ${lon}°`;
 
-    document.getElementById('temperature').innerHTML = `Temperature <br> ${tempF} K`; 
-    document.getElementById('feelsLike').innerHTML = `Feels Like <br> ${feels_likeF} K`;
-    document.getElementById('lowestTemp').innerHTML = `Lowest temp <br> ${min_tempF} K`;
-    document.getElementById('highestTemp').innerHTML = `Highest temp <br> ${max_tempF} K`;
+    document.getElementById("temperature").innerHTML = `Temperature <br> ${tempF} K`; 
+    document.getElementById("feelsLike").innerHTML = `Feels Like <br> ${feels_likeF} K`;
+    document.getElementById("lowestTemp").innerHTML = `Lowest temp <br> ${min_tempF} K`;
+    document.getElementById("highestTemp").innerHTML = `Highest temp <br> ${max_tempF} K`;
     
-    document.getElementById('pascal').innerHTML = `Pressure <br> ${api_pressure} Pa `;
-    document.getElementById('humidity').innerHTML = `Humidity <br> ${api_humidity}% `;
+    document.getElementById("pascal").innerHTML = `Pressure <br> ${api_pressure} Pa `;
+    document.getElementById("humidity").innerHTML = `Humidity <br> ${api_humidity}% `;
     
     
-    document.getElementById('speedAir').innerHTML = `Wind Speed <br> ${windSpeedApi} m/s`;
-    document.getElementById('degAir').innerHTML = `Wind Angle <br> ${windDegApi}°`;
+    document.getElementById("speedAir").innerHTML = `Wind Speed <br> ${windSpeedApi} m/s`;
+    document.getElementById("degAir").innerHTML = `Wind Angle <br> ${windDegApi}°`;
 
 
-    celsius.addEventListener('click', () => {
-        celsius.classList.add('chosen');
-        farenheit.classList.remove('chosen');
+    celsius.addEventListener("click", () => {
+        celsius.classList.add("chosen");
+        farenheit.classList.remove("chosen");
         
-        document.getElementById('temperature').innerHTML = `Temperature <br> ${(tempF - 273.15).toFixed(2)} °C`; 
-        document.getElementById('feelsLike').innerHTML = `Feels Like <br> ${(feels_likeF - 273.15).toFixed(2)} °C`;
-        document.getElementById('lowestTemp').innerHTML = `Lowest temp <br> ${(min_tempF - 273.15).toFixed(2)} °C`;
-        document.getElementById('highestTemp').innerHTML = `Highest temp <br> ${(max_tempF - 273.15).toFixed(2)} °C`;
+        document.getElementById("temperature").innerHTML = `Temperature <br> ${(tempF - 273.15).toFixed(2)} °C`; 
+        document.getElementById("feelsLike").innerHTML = `Feels Like <br> ${(feels_likeF - 273.15).toFixed(2)} °C`;
+        document.getElementById("lowestTemp").innerHTML = `Lowest temp <br> ${(min_tempF - 273.15).toFixed(2)} °C`;
+        document.getElementById("highestTemp").innerHTML = `Highest temp <br> ${(max_tempF - 273.15).toFixed(2)} °C`;
     });
-    farenheit.addEventListener('click', () => {
-        celsius.classList.remove('chosen');
-        farenheit.classList.add('chosen');    
+    farenheit.addEventListener("click", () => {
+        celsius.classList.remove("chosen");
+        farenheit.classList.add("chosen");    
         
-        document.getElementById('temperature').innerHTML = `Temperature <br> ${((tempF - 273.15)*(9/5)+32).toFixed(2)} F`; 
-        document.getElementById('feelsLike').innerHTML = `Feels Like <br> ${((feels_likeF - 273.15)*(9/5)+32).toFixed(2)} F`;
-        document.getElementById('lowestTemp').innerHTML = `Lowest temp <br> ${((min_tempF - 273.15)*(9/5)+32).toFixed(2)} F`;
-        document.getElementById('highestTemp').innerHTML = `Highest temp <br> ${((max_tempF - 273.15)*(9/5)+32).toFixed(2)} F`;
+        document.getElementById("temperature").innerHTML = `Temperature <br> ${((tempF - 273.15)*(9/5)+32).toFixed(2)} F`; 
+        document.getElementById("feelsLike").innerHTML = `Feels Like <br> ${((feels_likeF - 273.15)*(9/5)+32).toFixed(2)} F`;
+        document.getElementById("lowestTemp").innerHTML = `Lowest temp <br> ${((min_tempF - 273.15)*(9/5)+32).toFixed(2)} F`;
+        document.getElementById("highestTemp").innerHTML = `Highest temp <br> ${((max_tempF - 273.15)*(9/5)+32).toFixed(2)} F`;
     });
 }
 
